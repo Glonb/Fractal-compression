f485391c249e725ed4ce6ae98bbad0bda43abc20
wanyao <wanyaocsg17@icloud.com>
2021-04-28 11:10:22 +0800

    优化shell脚本语句

--- clean_all.sh
+++ clean_all.sh
@@ -1,33 +1,38 @@
 #!/bin/zsh
 
 rm outcome.bmp
-rm compressed.bmp
 rm modify.bmp
-rm graduate/Domain/*.bmp
-rm graduate/Range/*.bmp
-rm graduate/A/Range/first/*.bmp
-rm graduate/A/Range/second/*.bmp
-rm graduate/A/Range/third/*.bmp
-rm graduate/A/Range/forth/*.bmp
-rm graduate/B/first/*.bmp
-rm graduate/B/second/*.bmp
-rm graduate/B/third/*.bmp
-rm graduate/B/forth/*.bmp
-rm graduate/C/first/*.bmp
-rm graduate/C/second/*.bmp
-rm graduate/C/third/*.bmp
-rm graduate/C/forth/*.bmp
+rm graduate/B/newB.bmp
+rm graduate/C/newC.bmp
 rm graduate/encode.txt
 rm graduate/A/encode.txt
-rm out/production/demo/graduate/Range/*.bmp
-rm out/production/demo/graduate/Domain/*.bmp
 
+for t in Range Domain
+do
+  rm graduate/$t/*.bmp
+  rm out/production/demo/graduate/$t/*.bmp
+done
 
-for ((i=1; i < 9; i++)); do
+for p in first second third forth
+do
+  rm graduate/A/Range/$p/*.bmp
+  rm graduate/B/$p/*.bmp
+  rm graduate/C/$p/*.bmp
+  rm graduate/tamperDetection/A/Range/$p/*.bmp
+  rm graduate/tamperDetection/B/$p/*.bmp
+  rm graduate/tamperDetection/C/$p/*.bmp
+done
+
+for ((i=1; i < 9; i++))
+do
     rm graduate/TnDomain/"$i"/*.bmp
     rm graduate/A/Domain/"$i"/first/*.bmp
     rm graduate/A/Domain/"$i"/second/*.bmp
     rm graduate/A/Domain/"$i"/third/*.bmp
     rm graduate/A/Domain/"$i"/forth/*.bmp
+    rm graduate/tamperDetection/A/Domain/"$i"/first/*.bmp
+    rm graduate/tamperDetection/A/Domain/"$i"/second/*.bmp
+    rm graduate/tamperDetection/A/Domain/"$i"/third/*.bmp
+    rm graduate/tamperDetection/A/Domain/"$i"/forth/*.bmp
     rm out/production/demo/graduate/TnDomain/"$i"/*.bmp
 done
